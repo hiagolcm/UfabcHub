fb394e2c339474a43ba6b4f5aea1db9489f07a97
Melo-Silva HLC <hcardealdemelo@gmail.com>
2018-07-07 17:41:40 -0300

    Inicializa projeto utilizando start.sprint.io
    *Dependencias utilizadas:
    -JPA
    -Web
    -postgres
    -devtools
    -thymeleaf

---
+++ src/main/java/br/com/ufabchub/UfabcHub/UfabcHubApplication.java
@@ -0,0 +1,12 @@
+package br.com.ufabchub.UfabcHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UfabcHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UfabcHubApplication.class, args);
+	}
+}
