6dd22c02822d1c77d38e496e12be73593a930150
x-mizu <40467263+x-mizu@users.noreply.github.com>
2018-07-09 17:25:46 -0300

    Envia lista de disciplinas para a view

--- src/main/java/br/com/ufabchub/controller/ClassroomController.java
+++ src/main/java/br/com/ufabchub/controller/ClassroomController.java
@@ -3,6 +3,7 @@ package br.com.ufabchub.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.ufabchub.service.ClassroomService;
 
@@ -19,8 +20,10 @@ public class ClassroomController {
 	}
 	
 	@RequestMapping("addclassroom")
-	public String addclassroom() {
-		//Abre pagina para cadastro de Turmas
-		return "addclassroom";
+	public ModelAndView addclassroom() {
+		//Abre pagina para cadastro de Turmas e envia a lista de turmas para a página
+		ModelAndView mv = new ModelAndView("/addclassroom");
+		mv.addObject("classrooms", classrs.listAll());
+		return mv;
 	}
 }
