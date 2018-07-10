4a5b44210e5e9d2bea9689666e016dcb19a515d3
Melo-Silva HLC <hcardealdemelo@gmail.com>
2018-07-10 15:45:57 -0300

    Troca classe pai do Repository para JpaRepository
    JpaRepository eh filha da Crud e, portanto possui as mesmas
    funcionalidades da Crud e mais algumas

--- src/main/java/br/com/ufabchub/repository/ClassroomRepository.java
+++ src/main/java/br/com/ufabchub/repository/ClassroomRepository.java
@@ -1,9 +1,10 @@
 package br.com.ufabchub.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import br.com.ufabchub.model.Classroom;
 
-public interface ClassroomRepository extends CrudRepository<Classroom, Long>{
+public interface ClassroomRepository extends JpaRepository<Classroom, Long>{
 
 }
